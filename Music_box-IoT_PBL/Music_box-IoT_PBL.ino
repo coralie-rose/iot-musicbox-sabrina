c6f0821b85d742502481270bb375b33986ff5b17
coralie-rose <sabrina.fontaine@dawsoncollege.qc.ca>
2026-04-29 13:18:12 -0400

    Creation of Arduino sketch
    Version 0.1
    Only added header information and the libraries for the LCD display and joystick, which I already know I'll be using

---
+++ Music_box-IoT_PBL/Music_box-IoT_PBL.ino
@@ -0,0 +1,21 @@
+//  Music box PBL
+//  Version 0.1
+//  By Sabrina Fontaine
+//  Course: Introduction to IoT
+//  Dawson College
+//  Date: April 29th 2026
+
+#include <LiquidCrystal.h>
+#include <Joystick.h>
+
+
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
