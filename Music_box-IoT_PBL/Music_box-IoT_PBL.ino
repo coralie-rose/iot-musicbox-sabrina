caf35d3122899746be35827900e361a957ed74b9
coralie-rose <sabrina.fontaine@dawsoncollege.qc.ca>
2026-04-29 15:04:53 -0400

    Version 0.2
    Major changes:
    Added pin definitions (with actual pin #s to be added later)
    Created objects for the display and joystick
    Added object and pinMode initializations to setup
    
    Minor changes:
    Switched to a different joystick library

--- Music_box-IoT_PBL/Music_box-IoT_PBL.ino
+++ Music_box-IoT_PBL/Music_box-IoT_PBL.ino
@@ -1,18 +1,38 @@
 //  Music box PBL
-//  Version 0.1
+//  Version 0.2
 //  By Sabrina Fontaine
 //  Course: Introduction to IoT
 //  Dawson College
 //  Date: April 29th 2026
 
+//  Libraries
 #include <LiquidCrystal.h>
-#include <Joystick.h>
+#include <AlignedJoy.h>
 
+// Pin definitions  Will need to add pins once circuit built
+#define rs 
+#define en
+#define d4
+#define d5
+#define d6
+#define d7
+#define buttonPin
+#define buz1Pin
+#define buz2Pin
+#define joyX
+#define joyY
+
+//  Creation of objects
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+AlignedJoy joystick(joyX, joyY); 
 
 
 void setup() {
-  
-
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  pinMode(buz1Pin, OUTPUT);
+  pinMode(buz2Pin, OUTPUT);
 }
 
 void loop() {
