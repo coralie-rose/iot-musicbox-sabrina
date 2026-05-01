c34f6328002c4273121f2fe2ff7dd8dc4a1c0d9c
coralie-rose <sabrina.fontaine@dawsoncollege.qc.ca>
2026-04-30 20:40:01 -0400

    Version 0.3.1 - Joystick Read Testing
    Major changes:
    Created custom function songSelect to read the joystick values for selecting between the 4 different songs.
    Added console printing to test joystick and determine which values will need to be used
    
    Minor changes:
    Added Pin to the joystick pin names, to differentiate if want to use joyX and joyY as seperate variables
    Changed button to be a pullup input
    LCD and buzzer related lines are commented out temporarily to avoid compiler errors

--- Music_box-IoT_PBL/Music_box-IoT_PBL.ino
+++ Music_box-IoT_PBL/Music_box-IoT_PBL.ino
@@ -1,5 +1,5 @@
 //  Music box PBL
-//  Version 0.3
+//  Version 0.3.1
 //  By Sabrina Fontaine
 //  Course: Introduction to IoT
 //  Dawson College
@@ -17,28 +17,49 @@
 #define d5
 #define d6
 #define d7
-#define buttonPin
+#define buttonPin 4
 #define buz1Pin
-#define buz2Pin
-#define joyX
-#define joyY
+#define buz2Pin 
+#define joyXPin A0
+#define joyYPin A1
 
 //  Creation of objects
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-AlignedJoy joystick(joyX, joyY);
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+AlignedJoy joystick(joyXPin, joyYPin);
 Tone buz1();
 Tone buz2(); 
 
+//  Function declarations (Actually body of functions are placed after void loop)
+int songSelect();
 
 void setup() {
-  lcd.begin(16, 2);
+  //lcd.begin(16, 2);
   Serial.begin(9600);
-  buz1.begin(buz1Pin);
-  buz2.begin(buz2Pin);
-  pinMode(buttonPin, INPUT);
+  //buz1.begin(buz1Pin);
+  //buz2.begin(buz2Pin);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  //lcd.print("Select a song");
 }
 
 void loop() {
-  
+  Serial.println("Into loop");
+  int song = songSelect();
+
+}
+
+
+int songSelect(){
+  Serial.println("Into function");
+  while (digitalRead(buttonPin) == HIGH){
+    //  Serial printing to determine X and Y value directions and range
+    Serial.print("X pos.: ");
+    Serial.println(joystick.read(X));
+    Serial.print("Y pos.: ");
+    Serial.println(joystick.read(Y));
+    
+    delay(1000);
+  }
+  return 1;
 
 }
