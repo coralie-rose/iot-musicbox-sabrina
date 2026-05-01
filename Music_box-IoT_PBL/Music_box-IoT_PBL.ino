4140e3fb07f5d4c9a24bf735a3566969ad11e7d5
coralie-rose <sabrina.fontaine@dawsoncollege.qc.ca>
2026-05-01 10:43:31 -0400

    Version 0.4 - Functioning LCD
    Major changes:
    Assigned the LCD related pins
    Added the song names during the selection for display on LCD
    Switched serial messages in loop if else-if to LCD printing
    
    Minor changes:
    Changed button pin

--- Music_box-IoT_PBL/Music_box-IoT_PBL.ino
+++ Music_box-IoT_PBL/Music_box-IoT_PBL.ino
@@ -1,5 +1,5 @@
 //  Music box PBL
-//  Version 0.3.2
+//  Version 0.4
 //  By Sabrina Fontaine
 //  Course: Introduction to IoT
 //  Dawson College
@@ -11,20 +11,20 @@
 #include <Tone.h>
 
 // Pin definitions  Will need to add pins once circuit built
-#define rs 
-#define en
-#define d4
-#define d5
-#define d6
-#define d7
-#define buttonPin 4
+#define rs 8
+#define en 7
+#define d4 6
+#define d5 5
+#define d6 4
+#define d7 3
+#define buttonPin 2
 #define buz1Pin
 #define buz2Pin 
 #define joyXPin A0
 #define joyYPin A1
 
 //  Creation of objects
-// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 AlignedJoy joystick(joyXPin, joyYPin);
 Tone buz1();
 Tone buz2(); 
@@ -33,26 +33,27 @@ Tone buz2();
 int songSelect();
 
 void setup() {
-  //lcd.begin(16, 2);
+  lcd.begin(16, 2);
   Serial.begin(9600);
   //buz1.begin(buz1Pin);
   //buz2.begin(buz2Pin);
   pinMode(buttonPin, INPUT_PULLUP);
 
-  //lcd.print("Select a song");
+  lcd.print("Select a song");
 }
 
 void loop() {
   int song = songSelect();
   
+  // Determining if the user has made a valid selection
   if (song == 0){
-    //lcd.clear();
-    //lcd.print("No song selected");
-    Serial.println("No song selected");
+    lcd.clear();
+    lcd.print("No song selected");
   }
   else if (song > 0){
-    Serial.print("Song selected: ");
-    Serial.println(song);
+    lcd.clear();
+    lcd.print("Song selected: ");
+    lcd.println(song);
   }
 
   delay(1000);
@@ -63,27 +64,44 @@ void loop() {
 int songSelect(){
   int song = 0;
 
-  while (digitalRead(buttonPin) == HIGH){
+  while (digitalRead(buttonPin) == HIGH){   // Will stay in selection mode until confirmed with button press
     int joyX = joystick.read(X);
     int joyY = joystick.read(Y);
 
     //  Serial printing to determine X and Y value directions and range
-    /*  Serial.print("X pos.: ");
+    /* Serial.print("X pos.: ");
     Serial.println(joyX);
     Serial.print("Y pos.: ");
     Serial.println(joyY); */
     
+    //  Selection of song based on joystick values, while displaying name on LCD
     if (joyX > 900){
       song = 1;
+      lcd.clear();
+      lcd.print("Wet Hands");
+      lcd.setCursor(0, 1);
+      lcd.print("By C418");
     }
     else if (joyY < 10){
       song = 2;
+      lcd.clear();
+      lcd.print("Hummingberg");
+      lcd.setCursor(0, 1);
+      lcd.print("By Max LL");
     }
     else if (joyY > 800){
       song = 3;
+      lcd.clear();
+      lcd.print("Stardew Valley");
+      lcd.setCursor(0, 1);
+      lcd.print("Overture");
     }
     else if (joyX < 100){
       song = 4;
+      lcd.clear();
+      lcd.print("Song");  // To be changed once decided
+      lcd.setCursor(0, 1);
+      lcd.print("By someone");
     }
 
     delay(200);
